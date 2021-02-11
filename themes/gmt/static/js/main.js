203f82c75677d3676c083db6f033a6c3b9a99046
cferdinandi <chris@gomakethings.com>
2021-02-11 12:50:36 -0500

    Updated JS

--- themes/gmt/static/js/main.js
+++ themes/gmt/static/js/main.js
@@ -11,20 +11,20 @@
 	}
 
 	// Variables
-	var buyNow = document.querySelectorAll('.edd-buy-now-button');
+	let buyNow = document.querySelectorAll('.edd-buy-now-button');
 
 	// Handle "buy now" clicks
 	// Don't run if right-click or command/control + click
-	var buyNowHandler = function (event) {
+	function buyNowHandler (event) {
 		if (!event.target.classList.contains('edd-buy-now-button')) return;
 		if (event.button !== 0 || event.metaKey || event.ctrlKey) return;
 		event.target.innerHTML = 'Adding to cart...';
 		event.target.classList.add('disabled');
-	};
+	}
 
 	// Listen for "buy now" clicks
 	if (buyNow.length > 0) {
-		document.addEventListener('click', buyNowHandler, false);
+		document.addEventListener('click', buyNowHandler);
 	}
 
 	/**
@@ -33,43 +33,49 @@
 	 * @param {String} content  The content to add to the anchor link [default: #]
 	 * @param {String} styles   The class(es) to add to the link [default: anchor-link]
 	 */
-	var addHeadingLinks = function (selector, content, styles) {
+	function addHeadingLinks (selector, content, styles) {
 
 		// Make sure a selector was provided
 		if (!selector) return;
 
-		// Variables
-		var headings = Array.from(document.querySelectorAll(selector));
-		content = content || '#';
-		styles = styles || 'anchor-link';
+		// Get headings
+		let headings = document.querySelectorAll(selector);
 
-		headings.forEach(function (heading) {
-			if (!heading.id) return;
-			heading.innerHTML += ` <a class="${styles}" href="#${heading.id}">${content}</a>`;
-		});
+		// Create link
+		let link = document.createElement('a');
+		link.textContent = content || '#';
+		link.className = styles || 'anchor-link';
 
-	};
+		// Add link to headings
+		for (let heading of headings) {
+			if (!heading.id) continue;
+			let hLink = link.cloneNode(true);
+			hLink.href = `#${heading.id}`;
+			heading.append(hLink);
+		}
+
+	}
 
 	/**
 	 *
 	 * @param {Function} callback
 	 */
-	var mailchimp = function (callback) {
+	function mailchimp (callback) {
 
 		//
 		// Variables
 		//
 
 		// Fields
-		var form = document.querySelector('#mailchimp-form');
+		let form = document.querySelector('#mailchimp-form');
 		if (!form) return;
-		var email = form.querySelector('#mailchimp-email');
+		let email = form.querySelector('#mailchimp-email');
 		if (!email) return;
-		var status = form.querySelector('#mc-status');
-		var btn = form.querySelector('[data-processing]');
+		let status = form.querySelector('#mc-status');
+		let btn = form.querySelector('[data-processing]');
 
 		// Messages
-		var messages = {
+		let messages = {
 			empty: 'Please provide an email address.',
 			notEmail: 'Please use a valid email address.',
 			success: 'Success! Thanks for inviting me to your inbox.',
@@ -77,7 +83,7 @@
 		};
 
 		// Endpoint
-		var endpoint = 'https://gomakethings.com/checkout/wp-json/gmt-mailchimp/v1/subscribe';
+		let endpoint = 'https://gomakethings.com/checkout/wp-json/gmt-mailchimp/v1/subscribe';
 
 
 		//
@@ -89,21 +95,17 @@
 		 * @param  {Form}   form The form
 		 * @return {String}      The query string
 		 */
-		var serializeForm = function (form) {
-			var arr = [];
-			var formData = new FormData(form);
-			for (var key of formData.keys()) {
-				arr.push(encodeURIComponent(key) + '=' + encodeURIComponent(formData.get(key)));
-			}
-			return arr.join('&');
-		};
+		function serializeForm (form) {
+			let data = new FormData(form);
+			return new URLSearchParams(data).toString();
+		}
 
 		/**
 		 * Show a status message
 		 * @param  {String}  msg     The message to show
 		 * @param  {Boolean} success If true, the status was successful
 		 */
-		var showStatus = function (msg, success) {
+		function showStatus (msg, success) {
 
 			// Bail if there's no status container
 			if (!status) return;
@@ -115,13 +117,13 @@
 			status.className = success ? 'success-message' : 'error-message';
 			email.className = success ? '' : 'error';
 
-		};
+		}
 
 		/**
 		 * Send data to the API
 		 * @param  {String} params The form parameters
 		 */
-		var sendData = function (params) {
+		function sendData (params) {
 			fetch(endpoint, {
 				method: 'POST',
 				body: params,
@@ -133,7 +135,7 @@
 			}).then(function (data) {
 
 				// Show status
-				var success = data.code >= 200 && data.code < 300 ? true : false;
+				let success = data.code >= 200 && data.code < 300 ? true : false;
 				showStatus(success ? messages.success : data.message, success);
 
 				// If there's a callback, run it
@@ -146,12 +148,12 @@
 			}).finally(function () {
 				form.removeAttribute('data-submitting');
 			});
-		};
+		}
 
 		/**
 		 * Submit the form to the API
 		 */
-		var submitForm = function () {
+		function submitForm () {
 
 			// Add submitting state
 			form.setAttribute('data-submitting', true);
@@ -159,21 +161,21 @@
 			// Send the data to the MailChimp API
 			sendData(serializeForm(form));
 
-		};
+		}
 
 		/**
 		 * Validate the email address
 		 * @return {Boolean} If true, email is valid
 		 */
-		var isEmail = function () {
+		function isEmail () {
 			return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/.test(email.value);
-		};
+		}
 
 		/**
 		 * Validate the form fields
 		 * @return {Boolean} If true, form is valid
 		 */
-		var validate = function () {
+		function validate () {
 
 			// If no email is provided
 			if (email.value.length < 1) {
@@ -189,13 +191,13 @@
 
 			return true;
 
-		};
+		}
 
 		/**
 		 * Handle submit events
 		 * @param  {Event} event The event object
 		 */
-		var submitHandler = function (event) {
+		function submitHandler (event) {
 
 			// Stop form from submitting
 			event.preventDefault();
@@ -207,33 +209,33 @@
 			showStatus(btn.getAttribute('data-processing'), true);
 
 			// Validate email
-			var valid = validate();
+			let valid = validate();
 
 			if (valid) {
 				submitForm();
 			}
 
-		};
+		}
 
 
 		//
 		// Event Listeners & Inits
 		//
 
-		form.addEventListener('submit', submitHandler, false);
+		form.addEventListener('submit', submitHandler);
 
-	};
+	}
 
 	/**
 	 * Load pricing parity message
 	 */
-	var pricingParity = function (endpoint, template) {
+	function pricingParity (endpoint, template) {
 
 		// Make sure endpoint and template exist
 		if (!endpoint) return;
 
 		// Render the pricing parity message
-		var renderPricingParity = function (data) {
+		function renderPricingParity (data) {
 
 			// Make sure we have data and a template to render
 			if (!data || !template) return;
@@ -242,11 +244,11 @@
 			if (data.status === 'no_discount') return;
 
 			// Get the nav
-			var nav = document.querySelector('header');
+			let nav = document.querySelector('header');
 			if (!nav) return;
 
 			// Create container
-			var pricing = document.createElement('div');
+			let pricing = document.createElement('div');
 			pricing.id = 'pricing-parity';
 			pricing.className = 'bg-muted padding-top-small padding-bottom-small';
 			pricing.innerHTML = template.replace('{{iso}}', data.code).replace('{{country}}', data.country).replace('{{code}}', data.discount).replace('{{amount}}', data.amount);
@@ -254,10 +256,10 @@
 			// Insert into the DOM
 			nav.parentNode.insertBefore(pricing, nav);
 
-		};
+		}
 
 		// Get the pricing parity message via Ajax
-		var getPricingParity = function () {
+		function getPricingParity () {
 			fetch(endpoint).then(function (response) {
 				if (response.ok) {
 					return response.json();
@@ -272,17 +274,17 @@
 				renderPricingParity(data);
 
 			});
-		};
+		}
 
 		// Get and render pricing parity info
-		var pricing = sessionStorage.getItem('gmt-location-pricing');
+		let pricing = sessionStorage.getItem('gmt-location-pricing');
 		if (pricing) {
 			renderPricingParity(JSON.parse(pricing));
 		} else {
 			getPricingParity();
 		}
 
-	};
+	}
 
 	/**
 	 * Generate a table of contents from headings
@@ -292,19 +294,19 @@
 	 * @param  {String} styles           Any classes to add to the list nav
 	 * @param  {String} type             The list type (ul/ol)
 	 */
-	var tableOfContents = function (navSelector, headingsSelector, heading, styles, type) {
+	function tableOfContents (navSelector, headingsSelector, heading, styles, type) {
 
 		// Make sure a selector was provided
 		if (!navSelector || !headingsSelector) return;
 
 		// Get the nav
-		var nav = document.querySelector(navSelector);
+		let nav = document.querySelector(navSelector);
 		if (!nav) return;
 
 		// Variables
-		var headings = document.querySelectorAll(headingsSelector);
+		let headings = document.querySelectorAll(headingsSelector);
 		type = type || 'ul';
-		var navList = Array.from(document.querySelectorAll(headingsSelector)).map(function (heading) {
+		let navList = Array.from(document.querySelectorAll(headingsSelector)).map(function (heading) {
 			if (!heading.id) return '';
 			return `<li><a href="#${heading.id}">${heading.textContent}</a></li>`;
 		}).join('');
@@ -318,7 +320,7 @@
 			${navList}
 		</${type}>`;
 
-	};
+	}
 
 	// Mailchimp form
 	if (document.querySelector('#mailchimp-form')) {
